d3586b122d1d67baa3f0a5e73dbc90651b6f3e87
mike_karni <3923499@mail.ru>
2022-04-10 19:47:44 +0300

    added Random :wq

--- example3/Program.cs
+++ example3/Program.cs
@@ -1,3 +1,5 @@
-﻿int numberA = 113;
-int numberb = 10;
+﻿int numberA = new Random().Next(1,8);
+Console.WriteLine(numberA);
+int numberb = new Random().Next(0,15);
+Console.WriteLine(numberb);
 Console.WriteLine(numberA + numberb);
